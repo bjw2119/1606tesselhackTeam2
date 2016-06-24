946e6898d43a1307ab740ce9514d1fa85420adee
Barry Weinberg <bjw2119@gmail.com>
2016-06-24 14:53:01 -0400

    camera test

--- camera.js
+++ camera.js
@@ -4,3 +4,13 @@ var os = require('os');
 var http = require('http');
 var port = 8080;
 var camera = new av.Camera();
+
+http.createServer(function(req, res){
+  var takePicture = camera.capture();
+
+  takePicture.on('data', function(image){
+    res.writeHead(200, {'Content-Type': 'image/jpg'});
+    res.write(image);
+    res.end();
+  })
+}).listen(port, ()=> console.log('http://${os.hostname()}.local:${port}'));
